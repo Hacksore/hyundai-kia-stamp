69c98c8fcf4155a7a60f3c99936a4b60fcc79b4b
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-02-27 10:41:14 -0600

    Add CI/CD

--- src/main/java/com/stationdm/euapi/header/RemoteHttpHeader.java
+++ src/main/java/com/stationdm/euapi/header/RemoteHttpHeader.java
@@ -1,15 +1,14 @@
 package stationdm.euapi.header;
-
 import java.util.Base64;
 
 public class RemoteHttpHeader {
-    static {
-        System.loadLibrary("native-lib");
-    }
+	static {
+		System.loadLibrary("native-lib");
+	}
 
-    public static String getStamp(String payload) {
-        return Base64.getEncoder().encodeToString(stringFromJNI(payload));
-    }
+	public static String getStamp(String payload) {
+		return Base64.getEncoder().encodeToString(stringFromJNI(payload));
+	}
 
-    public static native byte[] stringFromJNI(String str);
+	public static native byte[] stringFromJNI(String str);
 }
