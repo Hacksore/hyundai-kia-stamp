993577b36ea7b8ad4f665704fd42def13c2b7507
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-06-08 20:49:58 -0500

    Refactor to use only 1K stamps

--- src/main/java/com/hyundai/stamp/Main.java
+++ src/main/java/com/hyundai/stamp/Main.java
@@ -3,6 +3,7 @@ import stationdm.euapi.header.RemoteHttpHeader;
 
 public class Main {
 
+	public static int MAX_STAMPS = 1000;
 	public static void main(String[] args) {
 		if(args.length == 0) {
 			System.out.println("Please give an input");
@@ -23,7 +24,7 @@ public class Main {
 
 		if (command.equalsIgnoreCase("list")) {
 			System.out.println("Generating 100k stamps...");
-			for(int i=0; i<100000; i++) {
+			for(int i=0; i < MAX_STAMPS; i++) {
 				int unixTime = (int) (System.currentTimeMillis() / 1000L) + i;
 				String stamp = RemoteHttpHeader.getStamp(clientId + ":" + unixTime);
 				System.out.println(stamp);
