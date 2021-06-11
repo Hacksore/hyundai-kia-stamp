8f45b3b134c5179403472afceec1101d914fa570
David Balan <david.balan@fr.clara.net>
2021-06-11 11:36:31 +0200

    feat(token): stop generating token in the future

--- src/main/java/com/hyundai/stamp/Main.java
+++ src/main/java/com/hyundai/stamp/Main.java
@@ -4,6 +4,7 @@ import hmns.global.mobile.application.MyApiConfig;
 public class Main {
 
 	public static int MAX_STAMPS = 1000;
+	public static int STEP_IN_SECONDS = 20;
 	public static void main(String[] args) {
 		if(args.length == 0) {
 			System.out.println("Please give an input");
@@ -18,15 +19,16 @@ public class Main {
 		String clientId = args[1];
 
 		if (command.equalsIgnoreCase("single")) {
-			String stamp = MyApiConfig.getStamp(clientId);
+			int now = (int) (System.currentTimeMillis() / 1000L);
+			String stamp = MyApiConfig.getStamp(clientId + ":" + now);
 			System.out.println(stamp);
 		}
 
 		if (command.equalsIgnoreCase("list")) {
 			System.out.println("Generating " + MAX_STAMPS + " stamps...");
-			int start = (int) (System.currentTimeMillis() / 1000L);
+			int start = (int) (System.currentTimeMillis() / 1000L) - (STEP_IN_SECONDS * MAX_STAMPS);
 			for(int i=0; i < MAX_STAMPS; i++) {
-				int unixTime = start + i * 30;
+				int unixTime = start + i * STEP_IN_SECONDS;
 				String stamp = MyApiConfig.getStamp(clientId + ":" + unixTime);
 				System.out.println(stamp);
 			}
